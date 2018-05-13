643f381945c4d7183286064b47f31fd8f8421a02
Jianru Deng <38848872+jdeng13@users.noreply.github.com>
2018-05-13 16:29:13 -0700

    Update mpi_hw3_1.c

--- mpi_hw3_1.c
+++ mpi_hw3_1.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[])  {
- int size, rank, next, prev, buf[2], tag1=1, tage2=2;
+ int size, rank, next, prev, buf[2], tag1=1, tag2=2;
  MPI_Request reqs[4];
- MPI_status stats[4];
+ MPI_Status stats[4];
  
  MPI_Init(&argc,&argv);
  MPI_Comm_size(MPI_COMM_WORLD, &size);
