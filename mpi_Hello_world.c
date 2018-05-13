2f0660b01a12f219fb761b92a84e0002bde31b5a
Jianru Deng <38848872+jdeng13@users.noreply.github.com>
2018-05-13 15:02:30 -0700

    Update and rename mpi_Hello_world to mpi_Hello_world.c

--- mpi_Hello_world.c
+++ mpi_Hello_world.c
@@ -8,7 +8,7 @@ int main (int argc, char* argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  printf("Hello, world! I am Computer Robot %d %d\n", rank, size);
+  printf("Hello, World! I am Computer Robot %d %d\n", rank, size);
   MPI_Finalize();
   
   return 0;
