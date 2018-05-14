c79f42e98c9b44793887003a55b9ed3f658c2b81
Jianru Deng <38848872+jdeng13@users.noreply.github.com>
2018-05-14 11:13:17 -0700

    Update mpi_ringtopo8.c

--- mpi_ringtopo8.c
+++ mpi_ringtopo8.c
@@ -24,12 +24,12 @@
 	MPI_Irecv(&buf[0], 1, MPI_INT, prev, tag1, MPI_COMM_WORLD, &reqs[0]);
 	MPI_Irecv(&buf[1], 1, MPI_INT, next, tag2, MPI_COMM_WORLD, &reqs[1]);
 	MPI_Irecv(&buf[2], 1, MPI_INT, next, tag3, MPI_COMM_WORLD, &reqs[2]);
-  MPI_Irecv(&buf[3], 1, MPI_INT, next, tag4, MPI_COMM_WORLD, &reqs[3]);
+        MPI_Irecv(&buf[3], 1, MPI_INT, next, tag4, MPI_COMM_WORLD, &reqs[3]);
 
 	MPI_Isend(&rank, 1, MPI_INT, prev, tag4, MPI_COMM_WORLD, &reqs[4]);
 	MPI_Isend(&rank, 1, MPI_INT, next, tag3, MPI_COMM_WORLD, &reqs[5]);
-  MPI_Isend(&rank, 1, MPI_INT, next, tag2, MPI_COMM_WORLD, &reqs[6]);
-  MPI_Isend(&rank, 1, MPI_INT, next, tag1, MPI_COMM_WORLD, &reqs[7]);
+        MPI_Isend(&rank, 1, MPI_INT, next, tag2, MPI_COMM_WORLD, &reqs[6]);
+        MPI_Isend(&rank, 1, MPI_INT, next, tag1, MPI_COMM_WORLD, &reqs[7]);
 	
 
 	MPI_Waitall(8, reqs, stats);
