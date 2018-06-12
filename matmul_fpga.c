4e7e591cde508bca4cefb6120180b298dba6b933
Jianru Deng <38848872+jdeng13@users.noreply.github.com>
2018-06-12 10:48:09 -0700

    Update matmul_fpga.c

--- matmul_fpga.c
+++ matmul_fpga.c
@@ -4,8 +4,8 @@ float C[DIM_SIZE] [DIM_SIZE];
 
 #pragma omp target device(fpga) \
     implements (block_matmul) \
-    copy_in(A,B) copy_inout(C)
-extern void matmul_fpga(float A[BS][BS],
+    copy_in(A,B) copy_inout(C)                 // specification of the direction of the arguments
+extern void matmul_fpga(float A[BS][BS],       // task offloading to fpga device
         float B[BS][BS], float C[BS][BS]);
 
 ...
@@ -14,9 +14,9 @@ int i, j ,k
 for (i = 0; i < DIM_SIZE; i++) {
     for (j = 0; j < DIM_SIZE; j++) {
         for (k = 0; k < DIM_SIZE; k++) {
-            #pragma omp block nest(3) factor(BS,BS,BS) \
-            #pragma omp task label(block_matmul)  \
-                              input(A[i][k],B[k][j])  \
+            #pragma omp block nest(3) factor(BS,BS,BS) \     //  blocking and packing/unpacking of data 
+            #pragma omp task label(block_matmul)  \          //  specification of the direction of the arguments
+                              input(A[i][k],B[k][j])  \      //  specification of the direction of the arguments
                               inout(C[i][j])
             C[i][j] += A[i][k] * B[k][j];
          }
